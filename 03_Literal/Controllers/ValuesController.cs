a88e1eccc9e986e487d37c0af1f789fb99df43e3
grandiosa iva <ggo24@yahoo.com>
2020-09-14 08:34:01 +0400

    1

---
+++ 03_Literal/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _03_Literal.Controllers
+{
+    public class ValuesController : Controller
+    {
+        public IActionResult List()
+        {
+            string[] data = {"List1", "List2", "List3"};
+            return View(data);
+        }
+    }
+}
