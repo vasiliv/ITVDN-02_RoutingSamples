a9e9fc55745c31f7396a349ffe0c1b13dcb9f1c5
grandiosa iva <ggo24@yahoo.com>
2020-09-15 11:56:45 +0400

    04_Variables

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cities = new Dictionary<string, string>(){
+                {"UK", "London, Manchester, Birmingham"},
+                {"USA", "Chicago, New York, Washington"},
+                {"India", "Mumbai, New Delhi, Pune"}
+            };
+            Console.WriteLine("Key: {0}, Value: {1}", cities.ElementAt(0).Key, cities.ElementAt(0).Value);
+            Console.ReadKey();
+        }
+    }
+}
