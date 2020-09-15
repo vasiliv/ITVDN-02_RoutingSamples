4ca6b92bbb38afaa461ec55dc03079d7b05f8d00
grandiosa iva <ggo24@yahoo.com>
2020-09-15 13:58:58 +0400

    05_Optional

---
+++ 05_Optional/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _05_Optional.Controllers
+{
+    public class HomeController : Controller
+    {
+        static private Dictionary<int, string> products = new Dictionary<int, string>();
+        public HomeController()
+        {
+            products.Add(1, "Laptop");
+            products.Add(2, "Desctop");
+            products.Add(3, "TV");
+        }
+        public IActionResult Details(int? Id)
+        {
+            return View(products[(int)Id] as object);
+        }
+    }
+}
